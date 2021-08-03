3c4865694464f458966952a9634e244d69f3a42c
Amrutha Srikanth <60552165+amruthas15@users.noreply.github.com>
2021-08-02 17:32:04 -0700

    [BookDetail Page][1/X] Added BookDetailsViewController to show details of book chosen

---
+++ Bookmark/View Controllers/BookDetailsViewController.h
@@ -0,0 +1,18 @@
+//
+//  BookDetailsViewController.h
+//  Bookmark
+//
+//  Created by Amrutha Srikanth on 8/2/21.
+//
+
+#import <UIKit/UIKit.h>
+#import "Book.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BookDetailsViewController : UIViewController
+@property (strong, nonatomic) NSString* googleBookID;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Bookmark/View Controllers/BookDetailsViewController.m
@@ -0,0 +1,31 @@
+//
+//  BookDetailsViewController.m
+//  Bookmark
+//
+//  Created by Amrutha Srikanth on 8/2/21.
+//
+
+#import "BookDetailsViewController.h"
+
+@interface BookDetailsViewController ()
+
+@end
+
+@implementation BookDetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"%@", self.googleBookID);
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
