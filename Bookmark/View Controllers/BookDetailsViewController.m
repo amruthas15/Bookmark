9603cd17dd83cfb3a225f8440898d66fd382ca30
Amrutha Srikanth <60552165+amruthas15@users.noreply.github.com>
2021-08-03 16:02:27 -0700

    [BookDetail Page][4/X] Added components to book details page

--- Bookmark/View Controllers/BookDetailsViewController.m
+++ Bookmark/View Controllers/BookDetailsViewController.m
@@ -6,8 +6,18 @@
 //
 
 #import "BookDetailsViewController.h"
+#import "Utilities.h"
+#import "APIManager.h"
 
 @interface BookDetailsViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *bookCoverImageView;
+@property (weak, nonatomic) IBOutlet UILabel *bookTitleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bookAuthorLabel;
+@property (weak, nonatomic) IBOutlet UILabel *publicationDateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *averageRatingLabel;
+@property (weak, nonatomic) IBOutlet UILabel *biographyLabel;
+@property (weak, nonatomic) IBOutlet UILabel *reviewCountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *listCountLabel;
 
 @end
 
@@ -16,6 +26,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"%@", self.googleBookID);
+    PFQuery *bookQuery = [Book query];
+    [bookQuery whereKey:@"googleBookID" equalTo: self.googleBookID];
+    [bookQuery orderByDescending:@"updatedAt"];
+    bookQuery.limit = 1;
+    
+    [bookQuery findObjectsInBackgroundWithBlock:^(NSArray * _Nullable books, NSError * _Nullable error) {
+        if (books) {
+            Book *book = [books firstObject];
+            self.bookCoverImageView.image = book.coverURL ? [Utilities getBookCoverImageFromString:book.coverURL] : [UIImage systemImageNamed:@"book"];
+            self.bookTitleLabel.text = book.bookTitle;
+            self.bookAuthorLabel.text = [Utilities getAuthorsOfBook:book.bookAuthors];
+            self.averageRatingLabel.text = [[book.avgRating stringValue] stringByAppendingString:@"★"];
+            self.reviewCountLabel.text = [book.numReviews stringValue];
+            self.listCountLabel.text = [book.numLists stringValue];
+        }
+        else {
+            NSLog(@"%@", error.localizedDescription);
+        }
+    }];
+    
+    [[APIManager shared] getBookInformation:self.googleBookID completion:^(NSDictionary *book, NSError *error) {
+        if (book) {
+            NSDictionary *volumeInfo = book[@"volumeInfo"];
+            self.publicationDateLabel.text = volumeInfo[@"publishedDate"];
+            NSAttributedString *bioText = [[NSAttributedString alloc] initWithData:[volumeInfo[@"description"] dataUsingEncoding:NSUTF8StringEncoding] options:@{NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType, NSCharacterEncodingDocumentAttribute: [NSNumber numberWithInt:NSUTF8StringEncoding]} documentAttributes:nil error:nil];
+            self.biographyLabel.text = [bioText string];
+
+        } else {
+            NSLog(@"%@", error.localizedDescription);
+        }
+    }];
 }
 
 /*
