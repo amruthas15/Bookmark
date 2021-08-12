968f44d8d14eba2a8b4b80777d76faa46555ed7a
Amrutha Srikanth <60552165+amruthas15@users.noreply.github.com>
2021-08-12 12:53:35 -0700

    [Profile Page][2/X] Created Profile feed in storyboard and created Profile Cell Class

---
+++ Bookmark/Views/ProfileCell.h
@@ -0,0 +1,16 @@
+//
+//  ProfileCell.h
+//  Bookmark
+//
+//  Created by Amrutha Srikanth on 8/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ProfileCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Bookmark/Views/ProfileCell.m
@@ -0,0 +1,23 @@
+//
+//  ProfileCell.m
+//  Bookmark
+//
+//  Created by Amrutha Srikanth on 8/12/21.
+//
+
+#import "ProfileCell.h"
+
+@implementation ProfileCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
